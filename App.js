97ec7f330b64d96c164d23740cccca765c09932f
js <liderja@naver.com>
2020-03-25 18:44:24 +0900

    init commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// react-natvigation 라이브러리 에서 StackNavigator 추가하기
+import { createStackNavigator } from 'react-navigation';
+// MainScreen 추가
+import MainScreen from './Components/MainScreen';
+ 
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppStackNavigator />
+    );
+  }
+}
+ 
+// StackNavigator 를 App에 추가하기
+const AppStackNavigator = createStackNavigator({
+  Main:{
+    screen: MainScreen
+  }
+})
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
